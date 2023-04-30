5416c2ec48e02210c0495c66dbfe22b12a46c783
dantheman <danogbo0@gmail.com>
2023-04-30 20:02:34 +0100

    recent commit

--- src/KineticEnergy.java
+++ src/KineticEnergy.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class KineticEnergy extends Physics {
 
     public KineticEnergy(double mass, double velocity) {
         super(mass, velocity);
     }
 
+    double inputMass;
 
+    public double calcKe(){
+        return 0.5*mass*velocity*velocity;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,14 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-//        System.out.print("Enter the mass of the object: ");
-//        double mass = scanner.nextDouble();
-//
-//        System.out.print("Enter the velocity of the object: ");
-//        double velocity = scanner.nextDouble();
-//
-//        System.out.print("Enter the height of the object: ");
-        double height = scanner.nextDouble();
+        System.out.print("Enter the value of mass: ");
+        double mass = scanner.nextDouble();
 
-        PotentialEnergy pe = new PotentialEnergy();
-        String peValue = pe.calcPE();
-        System.out.println("Potential energy: " + peValue);
+        System.out.print("Enter the value of velocity: ");
+        double velocity = scanner.nextDouble();
+
+        KineticEnergy ke = new KineticEnergy(mass, velocity);
+        System.out.println(ke.calcKe());
+
     }
 }
